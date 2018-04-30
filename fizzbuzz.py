6ca57d7d68014a0e00a844d70fb1acbb2b872005
agnesdonat <agi.donat@gmail.com>
2018-04-30 23:21:59 +0100

    Tests for buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,7 @@
 def returns_fizz(a):
     if a % 3 == 0:
       return "Fizz"
+
+def returns_buzz(a):
+    if a % 5 == 0:
+        return "Buzz"
--- unittesting_pytest/test_fizzbuzz.py
+++ unittesting_pytest/test_fizzbuzz.py
@@ -1,5 +1,9 @@
 import fizzbuzz
 
 def test_returns_fizz():
-    fizzbuzz.returns_fizz(1)
+    fizzbuzz.returns_fizz(3)
     assert "Fizz"
+
+def test_returns_buzz():
+    fizzbuzz.returns_buzz(5)
+    assert "Buzz"
